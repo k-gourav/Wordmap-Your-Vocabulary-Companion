e456da01538a450c1ad1cd96fca2688075d432cb
k-gourav <code2change.gourav@gmail.com>
2024-10-07 23:52:07 +0530

    feat: add initial structure for Dictionary and Header component

--- src/components/Dictionary/Dictionary.jsx
+++ src/components/Dictionary/Dictionary.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import styles from './Dictionary.module.css'
+import React from "react";
+import styles from "./Dictionary.module.css";
 
 const Dictionary = () => {
   return (
-    <div>Dictionary</div>
-  )
-}
+    <main className={styles.main__element}>
+      <div className={styles.main__word}>
+        <div className={styles.word__withphonetics}>
+          <h1>keyboard</h1>
+          <p>/'ki:b:cd/</p>
+        </div>
+        <button className={styles.audio__player}>
+          <img src="" alt="" />
+        </button>
+      </div>
+      <div className={styles.word__type}>
+        <h3>noun</h3>
+        <span className={styles.horizontal__ruler}></span>
+      </div>
+    </main>
+  );
+};
 
-export default Dictionary
+export default Dictionary;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header__element}>
+      <div className={styles.nav__bar}>
+        <img src="" alt="" />
 
-export default Header
+        <div className={styles.font_toggle__items}>
+          <select name="fonts" id={styles.font__type}>
+            <option value="serif">Serif</option>
+            <option value="sans-serif">Sans Serif</option>
+            <option value="monospace">Monospace</option>
+          </select>
+          <div className={styles.toggle__theme}>
+            <button></button>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.search__bar}>
+        <input type="text" name="search-bar" id={styles.search__bar} />
+        <img src="" alt="" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
