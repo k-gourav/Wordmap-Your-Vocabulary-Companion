e0d581f9166dfc55f0e886ff6f723bdde775ae2e
k-gourav <code2change.gourav@gmail.com>
2024-10-09 01:51:25 +0530

    feat: add responsive design for tablet and mobile layout

--- src/components/DictionaryWrapper/DictionaryWrapper.jsx
+++ src/components/DictionaryWrapper/DictionaryWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../Header/Header'
 import Dictionary from '../Dictionary/Dictionary'
 import Footer from '../Footer/Footer'
@@ -6,6 +6,14 @@ import Footer from '../Footer/Footer'
 const DictionaryWrapper = () => {
     const [fontSelected, setFontSelected] = useState('Serif')
     const [darkTheme, setDarkTheme] = useState(false);
+    
+    useEffect(() => {
+        document.body.setAttribute('data-theme', darkTheme ? 'dark' : 'light');
+    
+        return () => {
+          document.body.removeAttribute('data-theme');
+        };
+      }, [darkTheme]);
   return (
     <>
     <Header fontSelected={fontSelected} setFontSelected={setFontSelected} darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,44 @@
-import React, { useEffect } from "react";
+import React from "react";
 import dictionaryLogo from "../../assets/icons/dictionary-icon.svg";
 import moonLogo from "../../assets/icons/moon-logo.svg";
 import searchIcon from "../../assets/images/search-icon.svg";
 import styles from "./Header.module.css";
 
 const Header = ({ setFontSelected, fontSelected, darkTheme, setDarkTheme }) => {
-    const handleFontChange = (event) => {
-        setFontSelected(event.target.value);
-      };
-      const themeHandler = (event) => {
-        setDarkTheme(event.target.checked)
-      }
-      
-      useEffect(() => {
-        document.body.setAttribute('data-theme', darkTheme ? 'dark' : 'light');
-    
-        return () => {
-          document.body.removeAttribute('data-theme');
-        };
-      }, [darkTheme]);
+  const handleFontChange = (event) => {
+    setFontSelected(event.target.value);
+  };
+  const themeHandler = (event) => {
+    setDarkTheme(event.target.checked);
+  };
 
   return (
-    <header className={styles.header__element} style={{fontFamily: fontSelected}} data-theme={darkTheme ? "dark" : "light"}>
+    <header
+      className={styles.header__element}
+      style={{ fontFamily: fontSelected }}
+      data-theme={darkTheme ? "dark" : "light"}
+    >
       <div className={styles.nav__bar}>
         <img src={dictionaryLogo} alt="Dictionary-Logo" />
 
         <div className={styles.font_toggle__items}>
-          <select name="fonts" id={styles.font__type} onChange={handleFontChange}>
+          <select
+            name="fonts"
+            id={styles.font__type}
+            onChange={handleFontChange}
+          >
             <option value="Serif">Serif</option>
             <option value="Sans-Serif">Sans Serif</option>
             <option value="Monospace">Monospace</option>
           </select>
           <div className={styles.toggle__theme}>
             <label className={styles.switch}>
-              <input type="checkbox" id={styles.theme_toggle} onChange={themeHandler} checked={darkTheme} />
+              <input
+                type="checkbox"
+                id={styles.theme_toggle}
+                onChange={themeHandler}
+                checked={darkTheme}
+              />
               <span className={`${styles.slider} ${styles.round}`}></span>
             </label>
             <img src={moonLogo} alt="Moon-Logo" width="26" />
@@ -41,8 +46,15 @@ const Header = ({ setFontSelected, fontSelected, darkTheme, setDarkTheme }) => {
         </div>
       </div>
       <div className={styles.search__bar}>
-        <input type="text" name="search-bar" id={styles.search__input} placeholder="Search word..." />
-        <button className={styles.search__btn}><img src={searchIcon} alt="Search-Icon" /></button>
+        <input
+          type="text"
+          name="search-bar"
+          id={styles.search__input}
+          placeholder="Search word..."
+        />
+        <button className={styles.search__btn}>
+          <img src={searchIcon} alt="Search-Icon" />
+        </button>
       </div>
     </header>
   );
