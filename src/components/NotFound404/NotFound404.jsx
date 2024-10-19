db2bdfdb0b9b7347b3337d29d550df9e1ec1a4e7
k-gourav <code2change.gourav@gmail.com>
2024-10-20 01:07:25 +0530

    chore: fix the NotFound404 component

--- src/components/NotFound404/NotFound404.jsx
+++ src/components/NotFound404/NotFound404.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import error404 from "../../assets/images/error-404.png"
 import { Link } from 'react-router-dom';
-import styles from './Error404.module.css'; 
+import styles from './NotFound404.module.css'; 
 
 const NotFound404 = () => {
   return (
